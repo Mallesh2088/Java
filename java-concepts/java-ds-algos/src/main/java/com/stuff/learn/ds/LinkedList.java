4c65a0312a141f93ada11e10c5702746fab52499
Mallesh Nagarajan <mallesh2088@gmail.com>
2017-09-05 16:08:10 -0400

    Added functions for Linked List

--- java-concepts/java-ds-algos/src/main/java/com/stuff/learn/ds/LinkedList.java
+++ java-concepts/java-ds-algos/src/main/java/com/stuff/learn/ds/LinkedList.java
@@ -16,6 +16,7 @@ public class LinkedList {
 
 	public void printLinkedList() {
 		Node n = head;
+		System.out.println("Nodes of Linked List:: ");
 		while (n != null) {
 			System.out.println(n.data + "  ");
 			n = n.next;
@@ -31,6 +32,45 @@ public class LinkedList {
 		}
 		System.out.println("Length of Linked List:: " + counter);
 	}
+	
+	public void insertNodeAtFront(int data) {
+		Node newNode = new Node(data);
+		newNode.next = head;
+		head = newNode;
+	}
+	
+	public void insertNodeAtEnd(int data) {
+		Node newNode = new Node(data);
+		if(head == null) {
+			head = newNode;
+			return;
+		}
+		Node last = head;
+		while(last.next != null) {
+			last = last.next;
+		}
+		last.next = newNode;
+		
+	}
+	
+	public void insertNodeAfterGivenNode(Node node,int data) {
+		if(node == null) {
+			System.out.println("The given previous node cannot be null..");
+			return;
+		}
+		Node newNode = new Node(data);
+		Node prev_node = head;
+		while(prev_node.next != null) {
+			prev_node = prev_node.next;
+			if(prev_node.data == node.data) {
+				newNode.next = prev_node.next;
+				prev_node.next = newNode;
+				return;
+			}
+			
+		}
+		
+	}
 
 	public static void main(String[] args) {
 
@@ -40,6 +80,16 @@ public class LinkedList {
 		Node third = new Node(3);
 		linkedList.head.next = second;
 		second.next = third;
+		linkedList.printLinkedList();
+		linkedList.findLength();
+		linkedList.insertNodeAtFront(4);
+		linkedList.printLinkedList();
+		linkedList.findLength();
+		linkedList.insertNodeAtEnd(5);
+		linkedList.printLinkedList();
+		linkedList.findLength();
+		Node prevNode = new Node(3);
+		linkedList.insertNodeAfterGivenNode(prevNode, 6);
 		linkedList.printLinkedList();
 		linkedList.findLength();
 	}
