f4a2be22c91adf7416801be06cb330c6ec057ab4
Mallesh Nagarajan <mallesh2088@gmail.com>
2017-09-14 10:20:45 -0400

    Added functionality

--- java-concepts/java-ds-algos/src/main/java/com/stuff/learn/ds/LinkedList.java
+++ java-concepts/java-ds-algos/src/main/java/com/stuff/learn/ds/LinkedList.java
@@ -2,7 +2,7 @@ package com.stuff.learn.ds;
 
 public class LinkedList {
 
-	Node head;
+	static Node head;
 
 	static class Node {
 		int data;
@@ -12,6 +12,12 @@ public class LinkedList {
 			data = d;
 			next = null;
 		}
+
+		@Override
+		public String toString() {
+			return "Node [data=" + data + "]";
+		}
+
 	}
 
 	public void printLinkedList() {
@@ -23,53 +29,118 @@ public class LinkedList {
 		}
 	}
 	
+	public void printLinkedList(Node head) {
+		System.out.println("Nodes of Linked List:: ");
+		while (head != null) {
+			System.out.println(head.data + "  ");
+			head = head.next;
+		}
+	}
+
 	public void findLength() {
 		int counter = 0;
 		Node n = head;
 		while (n != null) {
-			counter ++;
+			counter++;
 			n = n.next;
 		}
 		System.out.println("Length of Linked List:: " + counter);
 	}
-	
+
+	public int findLengthRecursively(Node node) {
+		if (node == null)
+			return 0;
+		return 1 + findLengthRecursively(node.next);
+	}
+
+	public int findLengthRec() {
+		return findLengthRecursively(head);
+	}
+
 	public void insertNodeAtFront(int data) {
 		Node newNode = new Node(data);
 		newNode.next = head;
 		head = newNode;
 	}
-	
+
 	public void insertNodeAtEnd(int data) {
 		Node newNode = new Node(data);
-		if(head == null) {
+		if (head == null) {
 			head = newNode;
 			return;
 		}
 		Node last = head;
-		while(last.next != null) {
+		while (last.next != null) {
 			last = last.next;
 		}
 		last.next = newNode;
-		
+
 	}
-	
-	public void insertNodeAfterGivenNode(Node node,int data) {
-		if(node == null) {
+
+	public void insertNodeAfterGivenNode(Node node, int data) {
+		if (node == null) {
 			System.out.println("The given previous node cannot be null..");
 			return;
 		}
 		Node newNode = new Node(data);
 		Node prev_node = head;
-		while(prev_node.next != null) {
+		while (prev_node.next != null) {
 			prev_node = prev_node.next;
-			if(prev_node.data == node.data) {
+			if (prev_node.data == node.data) {
 				newNode.next = prev_node.next;
 				prev_node.next = newNode;
 				return;
 			}
-			
+
 		}
-		
+
+	}
+
+	public void deleteNode(int key) {
+		Node temp = head, prev = null;
+
+		// If given key is head
+		if (temp != null && temp.data == key) {
+			head = temp.next;
+			return;
+		}
+
+		// else traverse linked list
+		while (temp != null && temp.data != key) {
+			prev = temp;
+			temp = temp.next;
+		}
+
+		if (temp == null)
+			return;
+
+		prev.next = temp.next;
+	}
+
+	public void reverseLinkedList() {
+		Node current = head;
+		Node prev = null, next = null;
+		while (current != null) {
+			next = current.next;
+			current.next = prev;
+			prev = current;
+			current = next;
+		}
+		head = prev;
+	}
+	
+	public Node reverseListRecur(Node head) {
+	    if(head==null || head.next == null)
+	        return head;
+	 
+	    //get second node    
+	    Node second = head.next;
+	    //set first's next to be null
+	    head.next = null;
+	 
+	    Node rest = reverseListRecur(second);
+	    second.next = head;
+	    return rest;
 	}
 
 	public static void main(String[] args) {
@@ -92,6 +163,15 @@ public class LinkedList {
 		linkedList.insertNodeAfterGivenNode(prevNode, 6);
 		linkedList.printLinkedList();
 		linkedList.findLength();
+		linkedList.deleteNode(3);
+		linkedList.printLinkedList();
+		linkedList.findLength();
+		System.out.println("Length of Linked List Recursively :: " + linkedList.findLengthRec());
+		linkedList.reverseLinkedList();
+		linkedList.printLinkedList();
+		linkedList.findLength();
+		Node reverseListRecur = linkedList.reverseListRecur(head);
+		linkedList.printLinkedList(reverseListRecur);
 	}
 
 }
